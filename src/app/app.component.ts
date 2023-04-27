f17438f46be5d8eb3be2ae7957b6900f2b17f615
Manfredo Pérez <perezmanfredo959@gmail.com>
2023-04-27 09:46:33 -0600

    Version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Tarjeta{
+  titulo:string;
+  subtitulo:string;
+  nro?:number;
+}
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'MiprimerApp';
+  public ArregloTatjetas: Tarjeta[]=[]
+
+  ngOnInit(): void {
+
+    this.ArregloTatjetas = [
+      {titulo:'Video 1', subtitulo:'Subtitulo Video 1'},
+      {titulo:'Video 2', subtitulo:'Subtitulo Video 2'},
+      {titulo:'Video 3', subtitulo:'Subtitulo Video 3'},
+    ]
+  }
 }
